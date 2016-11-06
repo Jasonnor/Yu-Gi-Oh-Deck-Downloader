3ab07cfe3894c148d95bd0b56cd6c985fd78f0e9
Jasonnor <wujason810@gmail.com>
2016-11-06 16:27:46 +0800

    Grab the deck data

---
+++ js/deck-downloader.js
@@ -0,0 +1,7 @@
+﻿$(document).ready(function () {
+    var deckUrl = location.href.substr(location.href.lastIndexOf('/') + 1) + '&Text=5';
+    $.ajax(deckUrl).then(function (responseData) {
+        var deckData = responseData.substring(responseData.lastIndexOf('<pre>') + 5, responseData.lastIndexOf('</pre>'));
+        console.log(deckData);
+    });
+});
