ac2ca27a74e5de5d552cddd3e0c22d85646812d5
Jasonnor <wujason810@gmail.com>
2016-11-06 16:37:33 +0800

    Add download deck function

--- js/deck-downloader.js
+++ js/deck-downloader.js
@@ -1,7 +1,10 @@
 ﻿$(document).ready(function () {
     var deckUrl = location.href.substr(location.href.lastIndexOf('/') + 1) + '&Text=5';
+    var deckName = $('#s_result>a').html() + '-' + $('#main>h2').html().split('<span>')[0];
     $.ajax(deckUrl).then(function (responseData) {
         var deckData = responseData.substring(responseData.lastIndexOf('<pre>') + 5, responseData.lastIndexOf('</pre>'));
         console.log(deckData);
+        var blob = new Blob([deckData], {type: "text/plain;charset=utf-8"});
+        saveAs(blob, deckName + '.ydk');
     });
 });
